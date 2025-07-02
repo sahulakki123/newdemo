d1a94826c7264ddf55c3b5554794e2570812471f
Lakki sahu Sahu <lakkisahus04@gmail.com>
2025-07-02 14:14:07 +0530

    javascript

---
+++ frist.js
@@ -0,0 +1,16 @@
+console.log("inside js")
+document.write("<h1> hello </h1>")
+
+
+// varibles ->
+// var -> redeclare, reasing value, function scope
+// let - > not redeclare, reasing value, block scope
+
+// const - > not not block scope
+
+
+// ===============================================================
+
+
+
+
