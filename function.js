fe5e58bb80c2b417d37de75a1d112fdae87c084c
sahulakki123 <lakkisahus04@gmail.com>
2025-07-09 16:27:56 +0530

    function

---
+++ function.js
@@ -0,0 +1,112 @@
+// function -> block of code / set of instructions
+// uese  for code reuse
+
+
+
+
+// // syntex 
+// function identifire(){ -> function declaration
+//     // code -> function defention
+// }
+
+
+
+// identifire() -> function colling
+
+// function domo(){
+//     alert("working")
+// }
+// demo()
+
+
+
+
+// function add(){
+//     let a = parseInt(prompt("enter the 1 number"))
+//     let b = parseInt(prompt("enter the 2 number"))
+//     console.log(a+b)
+// }
+// add()
+// add()
+// add()
+
+
+// write a program to print eriar of crcal AND REDIYAS OF TEK AND USER
+// w a p difen gratasat number beetwwen number swc
+// w a p to print user name 10 times
+// w A P TO PTINT SUM OF ALL ENAN NUMBERS BETWEEN 1 TO 10 ARIMENTS TO SUT BE ARRY
+//  w A P TO PTINT KEYS OFAIN OBJECT OBJECT TO 5 VALUES 
+// 
+
+
+// // 1
+// function  js(){
+//     let r = parseInt(prompt("enter the number"))
+//     arr = 3.14*r*r
+//     console.log(arr)
+// }
+// js()
+
+
+// // 2
+// function fun(){
+//     num  = parseInt(prompt("enter the 1 number"))
+//     num = parseInt(prompt("enter the  2 number"))
+//     if(){
+
+//     }
+// }
+// fun()
+
+
+
+// // 3
+// function add(){
+
+// num  = parseInt(prompt("enter name"))
+// for(let b =0 ; b<10; b++)
+//     {
+//         console.log(num)
+//     }
+// }
+// add()
+
+
+
+// // 4
+
+// function n(){
+
+// let ar = [1,2,3,4,5,6,7,8,9,10]
+// let sum = 0
+// for(a = 0; a<10; a++){
+//     if (ar[a]%2 !=0){
+//         sum = sum + ar[a]
+//     }
+// }
+// console.log(sum)
+
+// }
+// n()
+
+
+
+// 5
+
+// function add(){
+// let ob = {
+//     name: "Lakki",
+//     contact: 401515,
+//     city: "bhopal",
+//     jd: "kdfib",
+//     q:"b.tech"
+
+// }
+
+// for(k in ob ){
+//     console.log(k)
+// }
+// }
+
+
+// 
