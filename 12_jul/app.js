5bd53af2c5d97021779c2c78a6eb6d717636c598
sahulakki123 <lakkisahus04@gmail.com>
2025-07-12 16:38:04 +0530

    string

---
+++ 12_jul/app.js
@@ -0,0 +1,12 @@
+function add(){
+    // alert("wellcom")
+
+    // let str =" this is bhopal which is best in bhopal"
+    // console.log(str.replace(/bhopal/gi ,"mp"))
+
+    let str = "this is string"
+    let trimtext=str.trim()
+
+    let text = "apple mengo  banana"
+    console.log(text.split(" _"))  
+}
