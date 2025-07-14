0a79ae5742f313fa4d5de2785e9fcd6a5890debf
sahulakki123 <lakkisahus04@gmail.com>
2025-07-14 17:09:15 +0530

    template

---
+++ 14_jul/app.js
@@ -0,0 +1,32 @@
+//  Template  literals
+
+// use 1
+
+let name ="lakki"
+let age = 20
+
+console.log(`My name is ${name} and i am ${age} years old`)
+
+// support multiline string (use 2)
+
+let n = `Lakki
+              sahu
+                  ji`
+console.log(n)
+
+
+// 3rd use => support html tage
+
+let names = "<h1>lakki </h1> sahu"
+
+document.write(names)
+
+// 4 => let 
+let use = `lakki 'okk' "ok" sahu`
+console.log(use)
+
+
+
+
+
+
