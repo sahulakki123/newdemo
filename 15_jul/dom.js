c19b6ccebba0c3983ee8206802734a26513457d4
sahulakki123 <lakkisahus04@gmail.com>
2025-07-15 16:54:26 +0530

    DOM

---
+++ 15_jul/dom.js
@@ -0,0 +1,39 @@
+// dom ak tree like starcher and chang ya remue 
+
+function addtext(){
+    // document.getElementById("text")
+    let T = document.querySelector("#text")
+    T.innerHTML="Thi is <i> javascript </i> class"
+    T.style.color = "red"
+    T.style.backgroundColor="lightblue"
+    // T.innertext="kjbk "
+}
+
+function change(){
+    let Tt = document.querySelector("#ok")
+    Tt.innerHTML="Thi is <i> django </i> class"
+    Tt.style.color = "red"
+
+
+    let Ttt = document.querySelector("#okk2")
+    // Ttt.innerHTML="Thi is <i> javascript </i> class"
+    Ttt.style.color = "blue"
+}
+
+
+function g(){
+    let Tt2 = document.querySelector("#p")
+    Tt2.style.color = "green"
+}
+
+function r(){
+    let Tt1 = document.querySelector("#p")
+    Tt1.style.color = "red"
+}
+
+
+
+
+// ak box hai  3 butan hai r
+
+// text ke pe cilik kare to red ho jaye 
