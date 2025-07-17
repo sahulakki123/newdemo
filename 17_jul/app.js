86666a946f3c07616546da1f27583aa3d8d8fd05
sahulakki123 <lakkisahus04@gmail.com>
2025-07-17 18:02:16 +0530

    img

---
+++ 17_jul/app.js
@@ -0,0 +1,37 @@
+function abc(){
+  let a =document.querySelector(".name").value
+  let b =document.querySelector(".name").value  
+  let sum = Number(a)+Number(b)
+  alert("sum : " +sum)
+}
+
+
+function changeimg(){
+  let a = document.querySelector(".cs1")
+  a.style.backgroundImage="url(moble.jpg)"
+}
+
+function changeimg1(){
+  let a = document.querySelector(".cs1")
+  a.style.backgroundImage="url(moble2.jpg)"
+}
+
+function changeimg2(){
+  let a = document.querySelector(".cs1")
+  a.style.backgroundImage="url(mobile3.jpg)"
+}
+
+function changeimg3(){
+  let a = document.querySelector(".cs1")
+  a.style.backgroundImage="url(mobile4.jpg)"
+}
+
+function changeimg4(){
+  let a = document.querySelector(".cs1")
+  a.style.backgroundImage="url(mobile5.jpg)"
+}
+
+function changeimg5(){
+  let a = document.querySelector(".cs1")
+  a.style.backgroundImage="url(mobile6.jpg)"
+}
