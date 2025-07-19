988df98a882f347f81e3849884f40a45b3e12968
sahulakki123 <lakkisahus04@gmail.com>
2025-07-19 16:26:33 +0530

    function

---
+++ 18_jul/function.js
@@ -0,0 +1,35 @@
+// // normal function
+
+// function add(){
+//     alert("hello")
+// }
+// add()
+
+// // iife function
+// (
+//     function(){
+//         alert("iife")
+//     }
+// )()
+
+// // annonymus function
+
+// let okkk = function(){
+//     alert("annonymus function")
+// }
+
+// // arrowc  function 
+
+// let okk= (a,b)=> {
+//     return a+b
+// }
+
+// let ok =(a,b)=> a+b
+// alert(okk(3,6))
+
+
+// calculate total marks and percentage 
+
+
+
+// counteraplication 
