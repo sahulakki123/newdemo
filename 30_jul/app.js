1173d776280a0d6b90286417b66f56cef6c0ee84
sahulakki123 <lakkisahus04@gmail.com>
2025-07-30 16:34:23 +0530

    date

---
+++ 30_jul/app.js
@@ -0,0 +1,37 @@
+// Date object 
+
+let time = new Date()
+
+console.log(time.getFullYear()); //2025
+console.log(time.getMonth()); //6 + 1 = 7
+console.log(time.getDate());
+console.log(time.getHours());
+console.log(time.getMinutes());
+console.log(time.getSeconds());
+console.log(time.getMilliseconds()); // 1000
+
+console.log(time.toLocaleString());
+
+
+let days = ['sun','mon','tue','wed','thr','fir','sat']
+
+console.log(days[time.getDay()]);
+
+
+let Months= ['des','jun','fab','mar','apr','may','jul','ag']
+
+console.log(Months[time.getMonth()]);
+
+
+
+let age = prompt("enter the age")
+let carent = time.getFullYear()-age
+console.log(carent);
+
+
+
+
+
+
+
+
