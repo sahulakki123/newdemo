0bff56aaf669be4e7785642411b92a7fadb7f91b
sahulakki123 <lakkisahus04@gmail.com>
2025-07-31 16:35:54 +0530

    math

---
+++ 31_jul/math.js
@@ -0,0 +1,30 @@
+// math
+
+// console.log(Math.floor(1+Math.random()*9));
+// console.log(Math.floor(Math.random()*9));
+// console.log(Math.floor(1000+Math.random()*9000));
+
+
+
+// console.log(Math.floor(1+Math.random()*6));
+
+// let  okk=()=>{
+//     setTimeout(()=>{
+//         alert("hii i am function")
+//     },3000)
+
+// }
+
+// setInterval(()=>{
+//     console.log("Lakki");
+    
+// },2000)
+
+let c=0
+let  okk1=()=>{
+    setInterval(()=>{
+      console.log(c);
+      c++   
+    },2000)
+
+}
