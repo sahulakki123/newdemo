7eb3d5a31f69b667f4f5bc4d6addeaf56e1f21d8
sahulakki123 <lakkisahus04@gmail.com>
2025-08-08 19:39:51 +0530

    crud

--- 5_aug/crud.js
+++ 5_aug/crud.js
@@ -1,3 +1,5 @@
+// fetch (GET ,POST, PUT, DELETE)
+
 let fatchdata= async()=>{
     let url = "http://localhost:3000/carbook"
 
@@ -26,5 +28,36 @@ let del=(id)=>{
 
     let url =`http://localhost:3000/carbook/${id}`
     fetch(url,{method:"DELETE"})
+
+
+
+let book=()=>{
+    let name = document.querySelector("#name").value
+    let car = document.querySelector("#car").value
+    let city = document.querySelector("#city").value
+    let p = document.querySelector("#p").value
+    let color = document.querySelector("#color").value
+
+    let url = "http://localhost:3000/carbook"
+
+    fetch(url,{
+
+        method:"POST",
+        headers:{
+            "Content-type":"application/json"
+        },
+        body: JSON.stringify({
+
+            name:name,
+            car:car,
+            city:city,
+            p:p,
+            color:color
+        })
+
+    })
+    location.href="crud.html"
+    return false
+}
 }
 fatchdata()
